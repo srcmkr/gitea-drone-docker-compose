a074a1ef05937ce3da79f54e32a6feb6c1835e6b
Benny <srcmkr@srcmkr.io>
2019-07-24 23:52:37 +0200

    Update docker-clean.sh

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,5 +1,10 @@
- 
-rm -rf data || true
-docker volume rm $(docker volume ls -q --filter dangling=true)
-docker system prune -a
-docker network create gitea-network
+read -r -p "Are you sure to destroy all container and persistent data? [Y/n] " response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]];
+then
+    docker-compose down
+    rm -rf data || true
+    docker volume rm $(docker volume ls -q --filter dangling=true)
+    docker system prune -a
+    docker network create gitea-network
+fi
